58d3bf5f2965f3d5f078b33eb387433050c49bc7
hgbeleza <developer.beleza06@gmail.com>
2024-11-13 16:57:19 -0700

    feat: creating the controller to add a new course

--- src/main/java/beleza/crudspring/controllers/CourseController.java
+++ src/main/java/beleza/crudspring/controllers/CourseController.java
@@ -3,22 +3,32 @@ package beleza.crudspring.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import beleza.crudspring.domain.dto.CourseDto;
 import beleza.crudspring.domain.model.Course;
-import beleza.crudspring.repository.CourseRepository;
+import beleza.crudspring.services.CourseService;
 
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
 
     @Autowired
-    private CourseRepository courseRepository;
+    private CourseService courseService;
 
     @GetMapping
     public List<Course> listAllCourses() {
-        return courseRepository.findAll();
+        return courseService.findAllCourses();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Course> insertCourse(@RequestBody CourseDto courseDto) {
+        courseService.insertCourse(courseDto);
+        return ResponseEntity.ok().build();
     }
 }
